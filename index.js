ac100df1cfaec04a723726bbb0468ea972c69a0b
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-02 13:11:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,26 +5,56 @@ function getCart() {
 }
 
 function setCart(c) {
-  cart = c;
+  
+
+cart = c;
   return cart;
 }
 
 function addToCart(item) {
  // write your code here
+ var itemAdd = { itemName: item , itemPrice: Math.floor(Math.random() * 100) + 1 }
+ cart.push(itemAdd)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  if ( cart.length === 0 ) {
+     return "Your shopping cart is empty."
+   } else {
+      const theCart = []
+      for (let i = 0; i < cart.length; i++) {
+       theCart.push(` ${cart[i].itemName} at $${cart[i].itemPrice}`)
+     }
+  if (cart.length === 1) {
+    return "In your cart, you have" + theCart + "."
+  } else {
+    const lastItem = theCart.pop()
+    return "In your cart, you have" + theCart + ", and" + lastItem + "."
+  }
+ }
 }
 
 function total() {
   // write your code here
+const thePrice =[]
+for (let i = 0; i < cart.length; i++) {
+       thePrice.push(cart[i].itemPrice)
+      }
+  return thePrice.reduce(function(a,b){
+    return a + b}, 0)
 }
 
 function removeFromCart(item) {
-  // write your code here
+  // write your code here 
+  if (item === cart.itemName) {
+    i = indexof(cart.itemName)
+    cart.splice(i, 1)
+  } else { return "That item is not in your cart." }
 }
 
 function placeOrder(cardNumber) {
   // write your code here
 }
+
